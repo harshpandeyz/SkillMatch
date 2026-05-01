262f2f7adf653df4618a70e306266dbb83951a84
harshpandeyz <Harshap17058@gmail.com>
2026-05-02 00:08:47 +0530

    config: add session store config

---
+++ backend/config/sessionStore.js
@@ -0,0 +1,25 @@
+const MySQLStore = require('express-mysql-session')(require('express-session'));
+const config = require('./env');
+
+const sessionStore = new MySQLStore({
+  host: config.database.host,
+  port: config.database.port,
+  user: config.database.user,
+  password: config.database.password,
+  database: config.database.database,
+  createDatabaseTable: true,
+  schema: {
+    tableName: 'user_sessions',
+    columnNames: {
+      session_id: 'session_id',
+      expires: 'expires',
+      data: 'data'
+    }
+  }
+});
+
+sessionStore.on('error', (error) => {
+  console.error('Session store error:', error.message);
+});
+
+module.exports = sessionStore;
